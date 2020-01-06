5d40c905eea24221514002aa5d8e36df42838bbc
Diana Uys <DianaU@saeon.local>
2020-01-06 14:20:51 +0200

    feat(ui) add fundingdetailstep page

---
+++ NCCRD_Client/app/js/components/Wizard/Steps/FundingDetailStep.jsx
@@ -0,0 +1,210 @@
+import React from 'react'
+import { Row, Col, Fa, Button } from 'mdbreact'
+import { connect } from 'react-redux'
+import TextComponent from '../../Shared/TextComponent.jsx'
+import SelectComponent from '../../Shared/SelectComponent.jsx'
+import { Popover } from 'antd'
+import _ from 'lodash'
+
+import "./shared.css"
+
+const mapStateToProps = (state, props) => {
+  let { lookupData: { users, fundingStatus } } = state
+  let { projectFundersData: { projectFunderDetails } } = state
+  return { users, fundingStatus, projectFunderDetails }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setFundingAction: payload => {
+      dispatch({ type: "ADD_PROJECTFUNDER_DETAILS", payload })
+    }
+  }
+}
+
+class FundingDetailStep extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    fundingStatus = this.fundingStatus.bind(this)
+
+  }
+
+  onAdd() {
+    let { addFundingAction, details } = this.props
+    addFundingAction({
+      id: details.FundingDetailId, 
+      state: 'modified'
+    })
+  }
+
+
+  onRemove() {
+    let { removeFundingAction, details, projectFunderDetails } = this.props
+    let actionIndex = projectFunderDetails.indexOf(details)
+    removeFundingAction(actionIndex)
+  }
+
+  render() {
+
+    let { details, users, fundingStatus } = this.props    
+    // let fundingStatus = this.state.bind(this)
+
+    return (
+      <>
+        <Row>
+          
+          <SelectComponent
+            col="col-md-6"
+            id="lblFundingStatus"
+            label="Funding Status:"
+            selectedValue={details.FundingStatusId}
+            data={fundingStatus}
+            setSelectedValueKey={"SET_PROJECTFUNDERS_FUNDINGSTATUS"}
+            parentId={details.FunderId}
+            dispatch={"LOAD_PROJECTFUNDERS_FUNDINGSTATUS"}
+            persist="FundingStatus"
+            allowEdit={false}
+            newItemTemplate={{
+              "Id": 0,
+              "Value": "",
+              "Description": ""
+            }}
+            allowClear={true}
+          />
+        </Row>
+
+        <div className="vertical-spacer" />
+
+        <Row>
+          <TextComponent
+            col="col-md-6"
+            id="lblGrantProgram"
+            label="Grant or Program name:"
+            value={details.GrantProgName}
+            setValueKey="SET_PROJECTFUNDER_GRANTPROGNAME"
+            parentId={details.FunderId}
+          />
+        </Row>
+
+        <div className="vertical-spacer" />
+
+        <Row>
+          <TextComponent
+            col="col-md-6"
+            id="lblFundingAgency"
+            label="Funding Agency:"
+            value={details.FundingAgency}
+            setValueKey="SET_PROJECTFUNDER_FUNDINGAGENCY"
+            parentId={details.FunderId}
+          />
+        </Row>
+
+        <div className="vertical-spacer" />
+
+        <Row>
+          <TextComponent
+            col="col-md-6"
+            id="lblPartneringDepts"
+            label="Partnering Departments/Organisations:"
+            value={details.PartnerDepsOrgs}
+            setValueKey="SET_PROJECTFUNDER_PARTNERDEPSORGS"
+            parentId={details.FunderId}
+          />
+        </Row>
+
+        <div className="vertical-spacer" />
+
+        <Row>
+          <SelectComponent
+            col="col-md-6"
+            id="lblProjectCoordinator"
+            label="Project Coordinator:"
+            readOnly={true}
+            selectedValue={details.ProjectCoordinatorId}
+            data={users.map(x => { return { Id: x.PersonId, Value: (x.FirstName + " " + x.Surname + " (" + x.EmailAddress + ")") } })}
+            setSelectedValueKey={"SET_PROJECTFUNDERS_PROJECTCOORDINATOR"}
+            parentId={details.FunderId}
+            dispatch={"LOAD_PROJECTFUNDERS_PROJECTCOORDINATOR"}
+            persist="ProjectCoordinator"
+            allowEdit={false}
+            newItemTemplate={{
+              "Id": 0,
+              "Value": "",
+              "Description": ""
+            }}
+            allowClear={true}
+          />
+        </Row>
+
+        <div className="vertical-spacer" />
+
+        <Row>
+          <TextComponent
+            col="col-md-6"
+            id="lblTotalBudget"
+            label="Total Budget:"
+            value={details.TotalBudget}
+            setValueKey="SET_PROJECTFUNDER_TOTALBUDGET"
+            parentId={details.FunderId}
+            numeric={true}
+          />
+        </Row>
+
+        <div className="vertical-spacer" />
+
+        <Row>
+          <TextComponent
+            col="col-md-6"
+            id="lblAnnualBudget"
+            label="Annual Budget:"
+            value={details.AnnualBudget}
+            setValueKey="SET_PROJECTFUNDER_ANNUALBUDGET"
+            parentId={details.FunderId}
+            numeric={true}
+          />
+        </Row>
+
+        <div className="vertical-spacer" />
+
+        <Row>
+          <SelectComponent
+            col="col-md-6"
+            id="lblFundingStatus"
+            label="Funding Status:"
+            selectedValue={details.FundingStatusId}
+            data={fundingStatus}
+            setSelectedValueKey={"SET_PROJECTFUNDERS_FUNDINGSTATUS"}
+            parentId={details.FunderId}
+            dispatch={"LOAD_PROJECTFUNDERS_FUNDINGSTATUS"}
+            persist="FundingStatus"
+            allowEdit={false}
+            newItemTemplate={{
+              "Id": 0,
+              "Value": "",
+              "Description": ""
+            }}
+            editModeOverride={true}
+            allowClear={false}
+          />
+        </Row>
+
+        {/* <div className="vertical-spacer" />
+
+        <Row>
+          <Col>
+            <Popover content={"Remove this funding action completely"}>
+              <Button className="inline-button" color="danger" onClick={this.onRemove}>
+                <Fa className="button-icon" icon="minus-circle" />
+                Remove funding
+              </Button>
+            </Popover>
+          </Col>
+        </Row> */}
+      </>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FundingDetailStep)
